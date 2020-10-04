d6fc7676d75d77a1a7da2d2f3b50320d54d9d516
ZzzNeko <547592574@qq.com>
2020-10-04 11:25:45 +0800

    笔记暂存

--- .vuepress/configs/network.js
+++ .vuepress/configs/network.js
@@ -2,11 +2,13 @@ module.exports = {
   title: "网络原理",
   themeConfig: {
     nav: [
+      { text: "网络模型", link: "/network/model/" },
       { text: "应用层", link: "/network/application/" },
       { text: "传输层", link: "/network/transport/" },
       // { text: "网络层", link: "/network/network/" },
     ],
     sidebar: {
+      // "/network/model/": [["./", "网络模型"]],
       "/network/application/": [
         ["HTTP", "HTTP"],
         ["DNS", "DNS"],
