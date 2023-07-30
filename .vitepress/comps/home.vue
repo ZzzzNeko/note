2582068868ad490cbc7f8931840c626c1ed1a183
ZzzzNeko <547592574@qq.com>
2023-07-30 16:12:44 +0800

    feat: 首屏增加 loading

--- .vitepress/comps/home.vue
+++ .vitepress/comps/home.vue
@@ -1,19 +1,28 @@
 <template>
-<div class="home">
-  <Graph class="graph-tree" :render-data="tree1" @click-leaf="routerTo" />
-  <Graph class="graph-tree" :render-data="tree2" @click-leaf="routerTo" />
-  <Graph class="graph-tree" :render-data="tree3" @click-leaf="routerTo" />
-  <Graph class="graph-tree" :render-data="tree4" @click-leaf="routerTo" />
-  <Graph class="graph-tree" :render-data="tree5" @click-leaf="routerTo" />
-  <Graph class="graph-tree" :render-data="tree6" @click-leaf="routerTo" />
-  <Graph class="graph-tree" :render-data="tree7" @click-leaf="routerTo" />
-  <Graph class="graph-tree" :render-data="tree8" @click-leaf="routerTo" />
-</div>
+  <Suspense>
+    <div class="home">
+      <Graph class="graph-tree" :render-data="tree1" @click-leaf="routerTo" />
+      <Graph class="graph-tree" :render-data="tree2" @click-leaf="routerTo" />
+      <Graph class="graph-tree" :render-data="tree3" @click-leaf="routerTo" />
+      <Graph class="graph-tree" :render-data="tree4" @click-leaf="routerTo" />
+      <Graph class="graph-tree" :render-data="tree5" @click-leaf="routerTo" />
+      <Graph class="graph-tree" :render-data="tree6" @click-leaf="routerTo" />
+      <Graph class="graph-tree" :render-data="tree7" @click-leaf="routerTo" />
+      <Graph class="graph-tree" :render-data="tree8" @click-leaf="routerTo" />
+    </div>
+    <template #fallback>
+      <div class="loading-wrap">
+        <Loading>加载中...</Loading>
+      </div>
+    </template>
+  </Suspense>
 </template>
 
 <script lang='ts' setup>
-import { ref, shallowRef, reactive, computed, watch, watchEffect, onMounted } from 'vue'
-import Graph from './tree/Tree'
+import { defineAsyncComponent } from 'vue'
+import Loading from './loading.vue'
+// import Graph from './tree/Tree'
+const Graph = defineAsyncComponent(() => import('./tree/Tree'))
 import { tree1, tree2, tree3, tree4, tree5, tree6, tree7, tree8 } from '../sidebar'
 import { useRouter, useData, withBase } from 'vitepress'
 // const siteData = useData()
@@ -35,5 +44,15 @@ const routerTo = (link: string) => router.go(withBase(link))
   width: calc(25% - 10px);
   margin: 4px;
   box-shadow: 2px 2px 4px 2px rgba(208, 191, 255, 0.6);
+}
+.loading-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ .vitepress/comps/loading.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="loading">
+    <div class="spinner">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+    <div class="content"><slot /></div>
+  </div>
+</template>
+
+<script lang='ts' setup>
+// import { ref, shallowRef, reactive, computed, watch, watchEffect, onMounted } from 'vue'
+// interface Props {
+//   size: 'small' | 'normal' | 'large'
+// }
+// const props = withDefaults(defineProps<Props>(), {
+//   size: 'normal'
+// })
+</script>
+
+<style scoped>
+.spinner {
+  width: 160px;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+}
+.content {
+  font-size: 16px;
+  line-height: 32px;
+  text-align: center;
+  color: #d0bfff;
+}
+ 
+.spinner > div {
+  background-color: #d0bfff;
+  height: 100%;
+  width: 20px;
+  display: inline-block;
+   
+  -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;
+  animation: sk-stretchdelay 1.2s infinite ease-in-out;
+}
+ 
+.spinner .rect2 {
+  -webkit-animation-delay: -1.1s;
+  animation-delay: -1.1s;
+}
+ 
+.spinner .rect3 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+ 
+.spinner .rect4 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+ 
+.spinner .rect5 {
+  -webkit-animation-delay: -0.8s;
+  animation-delay: -0.8s;
+}
+ 
+@-webkit-keyframes sk-stretchdelay {
+  0%, 40%, 100% { -webkit-transform: scaleY(0.4) }  
+  20% { -webkit-transform: scaleY(1.0) }
+}
+ 
+@keyframes sk-stretchdelay {
+  0%, 40%, 100% { 
+    transform: scaleY(0.4);
+    -webkit-transform: scaleY(0.4);
+  }  20% { 
+    transform: scaleY(1.0);
+    -webkit-transform: scaleY(1.0);
+  }
+}
+/* @keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.loading-circle{
+  border-style: solid;
+  border-color: #000;
+  border-bottom-color: #FF3D00;
+  border-radius: 50%;
+  display: inline-block;
+  animation: rotation 1.5s linear infinite;
+  -webkit-animation: rotation 1.5s linear infinite;
+}
+.loading-circle.-small {
+  width: 64px;
+  height: 64px;
+  border-width: 8px;
+}
+.loading-circle.-normal {
+  width: 96px;
+  height: 96px;
+  border-width: 12px;
+}
+.loading-circle.-large {
+  width: 128px;
+  height: 128px;
+  border-width: 16px;
+} */
+</style>
